f1f5164d1fa2915e75405efe27fa344b2a036672
Gustavo-Developer <srto.mendes@gmail.com>
2022-08-04 21:35:39 -0300

    :tada: Config Repositories

--- src/main/java/com/gustavo/bookstore/domain/Livro.java
+++ src/main/java/com/gustavo/bookstore/domain/Livro.java
@@ -15,7 +15,6 @@ import javax.persistence.ManyToOne;
 public class Livro implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +22,7 @@ public class Livro implements Serializable {
 	private String titulo;
 	private String nome_autor;
 	private String texto;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
